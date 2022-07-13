f310836f3c45123b5515d4805d189ca513a28a06
Michael Swoboda <pv2swoboda@gmail.com>
2022-07-12 21:00:26 -0500

    Add project files.

---
+++ IntroToCSharpProject/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+
+Random randsomNumberGenerator = new Random();
+int numberToGuess =randsomNumberGenerator.Next(1, 101);
+
+
+
+Console.Write("Pick a number between 1 and 100:");
+string input = Console.ReadLine();
+int inputAsInt = int.Parse(input);
+
+
+bool correctGuess = false;
+if (numberToGuess == inputAsInt)
+{
+    correctGuess = true;
+}
+
+string correctResponse = $"That's right, the number I was Thinking of was {numberToGuess}";
+string IncorrectGuess = $"Wow you're bad at this. I was thinking of {numberToGuess} but you guessed {input}.";
+
+if (correctGuess)
+{
+    Console.WriteLine(correctResponse);
+}
+else
+{
+    Console.WriteLine(IncorrectGuess);
+}
+
+Console.Write("I was thinking of this number but you picked that other number.");
+
+
+
+Console.ReadLine();
